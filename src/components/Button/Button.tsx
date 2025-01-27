ccbad6e97e7f3cc0745d2fdb0450ae03d3b85424
IslamSolimanInnowise <islam.soliman@innowise.com>
2025-01-27 15:21:00 +0300

    feat: add Button component

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,20 @@
+import classes from "./button.module.css";
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: "text" | "contained" | "outlined";
+  color?: "primary" | "secondary" | "error";
+  children?: React.ReactNode;
+}
+
+const Button: React.FC<ButtonProps> = ({
+  variant = "contained",
+  color = "",
+  children,
+}) => {
+  return (
+    <button className={`${classes.btn} ${classes[variant]} ${classes[color]}`}>
+      {children}
+    </button>
+  );
+};
+export default Button;
