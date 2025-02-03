805981e7cfb4d967bc5dc3133c7a39ad0a8aadaf
IslamSolimanInnowise <islam.soliman@innowise.com>
2025-02-03 15:33:04 +0300

    test: add onChange test

--- src/components/Switch/Switch.test.tsx
+++ src/components/Switch/Switch.test.tsx
@@ -1,6 +1,18 @@
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 import Switch from "./Switch";
+import { useState } from "react";
+
+const Wrapper = () => {
+  const [switchState, setSwitchState] = useState(false);
+
+  function handleSwitchChange() {
+    setSwitchState((prev) => !prev);
+    console.log("changed");
+  }
+
+  return <Switch checked={switchState} onChange={handleSwitchChange} />;
+};
 
 describe("Switch", () => {
   test("renders Switch with default props", () => {
@@ -34,5 +46,17 @@ describe("Switch", () => {
     const switchElement = screen.getByRole("switch");
     expect(switchElement).toBeInTheDocument();
     expect(switchElement).toHaveClass("pointer-events-none");
+  });
+
+  test("renders Switch with onChange prop", async () => {
+    render(<Wrapper />);
+
+    const switchElement = screen.getByRole("switch");
+    expect(switchElement).toBeInTheDocument();
+
+    await switchElement.click();
+
+    expect(switchElement).toHaveClass("bg-blue-600");
+    expect(switchElement).not.toHaveClass("bg-gray-300");
   });
 });
